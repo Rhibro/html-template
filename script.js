2bae29dd97fd4fe37542e9a87693702bdea93764
Rhibro <111956763+Rhibro@users.noreply.github.com>
2022-11-21 14:26:55 +0100

    Triangles

---
+++ script.js
@@ -0,0 +1,37 @@
+let triangleOne = (10 * 13) / 2;
+console.log(triangleOne);
+
+let triangleTwo = (16.5 * 20.3) / 2;
+console.log(triangleTwo);
+
+if (triangleOne > triangleTwo) {
+   console.log("Triangle One is biggest!")
+} else if (triangleTwo > triangleOne) {
+    console.log("Triangle Two is biggest!")
+}
+
+let triangleThree = (16.5 * 20.3) / 2;
+console.log(triangleThree);
+
+let triangleFour = (20.3 * 16.5) /2;
+console.log(triangleFour);
+
+if (triangleThree > triangleFour) {
+    console.log("Triangle Three is biggest!");
+} else if (triangleFour > triangleThree) {
+    console.log("Triangle Four is biggest!");
+} else if (triangleFour === triangleThree) {
+    console.log("Triangle Four and Five are equal in size!");
+}
+
+let triangleFive = (7.8 * 5.6) / 2;
+console.log(triangleFive);
+
+let triangleSix = (9.3 * 12.4) / 2;
+console.log(triangleSix);
+
+if (triangleFive > triangleSix) {
+    console.log("Triangle Five is biggest!")
+} else if (triangleSix > triangleFive) {
+    console.log("Triangle Six is biggest!")
+}
